77b5826ddcaa8c922fcd9f3943bbaa379fc51886
AlanPonce11 <45779828+AlanPonce11@users.noreply.github.com>
2023-04-06 19:03:18 -0600

    Add files via upload

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200,
@@ -12,6 +13,9 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                       args=[self.slug])
 class Product(models.Model):
     category = models.ForeignKey(Category,
                                  related_name='products',
@@ -35,5 +39,8 @@ class Product(models.Model):
         ]
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id, self.slug])
 
 # Create your models here.
